326cf67744f29b615e923acbc9b5fa94e8444a14
faruqfadhil <faruqfadhil7@gmail.com>
2020-03-08 21:41:47 +0700

    merge sort

--- sorting/sort.go
+++ sorting/sort.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	list := []int{30, 62, 53, 42, 17, 97, 91, 38}
 	// fmt.Println(insertionSort(list))
-	// fmt.Println(selectionSort(list))
+	fmt.Println(selectionSort(list))
 	// fmt.Println(bubbleSort(list))
-	fmt.Println(shellSort(list))
+	// fmt.Println(shellSort(list))
 }
 
 func insertionSort(nums []int) []int {
@@ -28,12 +28,14 @@ func insertionSort(nums []int) []int {
 func selectionSort(nums []int) []int {
 	for i := 0; i < len(nums); i++ {
 		smallestIdx := i
-		for j := i; j < len(nums); j++ {
+		for j := i + 1; j <= len(nums)-1; j++ {
 			if nums[smallestIdx] > nums[j] {
 				smallestIdx = j
 			}
 		}
-		nums[i], nums[smallestIdx] = nums[smallestIdx], nums[i]
+		if nums[i] != nums[smallestIdx] {
+			nums[i], nums[smallestIdx] = nums[smallestIdx], nums[i]
+		}
 	}
 
 	return nums
@@ -73,4 +75,38 @@ func shellSort(nums []int) []int {
 		}
 	}
 	return nums
+}
+
+func mergeSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+
+	mid := len(nums) / 2
+	leftSubArray := nums[:mid]
+	rightSubArray := nums[mid:]
+	return merge(mergeSort(leftSubArray), mergeSort(rightSubArray))
+}
+
+func merge(left, right []int) []int {
+	size := len(left) + len(right)
+	l := 0
+	r := 0
+	newSlice := make([]int, size)
+	for i := 0; i < size; i++ {
+		if l > len(left)-1 && r <= len(right)-1 {
+			newSlice[i] = right[r]
+			r++
+		} else if r > len(right)-1 && l <= len(left)-1 {
+			newSlice[i] = left[l]
+			l++
+		} else if right[r] < left[l] {
+			newSlice[i] = right[r]
+			r++
+		} else {
+			newSlice[i] = left[l]
+			l++
+		}
+	}
+	return newSlice
 }
